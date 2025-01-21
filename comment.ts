bd427d94983c8fdd66ae8476bc769746a65ec8a5
realkyr <phuree.kanusont@gmail.com>
2025-01-21 19:53:46 +0700

    finish comment module

---
+++ comment.ts
@@ -0,0 +1,17 @@
+// {
+//   "id": "AVv9erhj5BA7J7JrOC11",
+//   "content": "อร่อยดีจ้า 2",
+//   "createdAt": "2025-01-21T12:51:56.324Z",
+//   "updatedAt": "2025-01-21T12:51:56.324Z",
+//   "author": "system",
+//   "postId": "moJnAZ5HQUjZpraqBkqT"
+// }
+
+export interface CommentType {
+  id: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  author: string;
+  postId: string;
+}
--- pagination.ts
+++ pagination.ts
@@ -1,5 +1,5 @@
 export interface Pagination {
   total: number;
   limit: number;
-  offset: number;
+  page: number;
 }
