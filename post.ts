47de068fb6faddb48065bfb0319e50645d51b437
realkyr <phuree.kanusont@gmail.com>
2025-01-21 10:49:07 +0700

    update shared-type

--- post.ts
+++ post.ts
@@ -1,16 +1,3 @@
-import { firestore } from "firebase-admin";
-import Timestamp = firestore.Timestamp;
-
-export interface PostDocument {
-  id: string;
-  community: string;
-  content: string;
-  createdAt: Timestamp;
-  createdBy: string;
-  title: string;
-  updatedAt: Timestamp;
-}
-
 export interface Post {
   id: string;
   community: string;
